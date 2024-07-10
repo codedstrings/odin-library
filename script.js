995f4de64edada62375ca881433df480e8492107
codedstrings <mridhulml@gmail.com>
2024-07-10 23:04:43 +0530

    add basic dialgoue modal for adding new book

--- script.js
+++ script.js
@@ -2,35 +2,46 @@ import { addBookToLibrary, myLibrary } from "./module/libraryscript.js";
 
 const librarySection = document.querySelector(".myBooks");
 const addButton = document.querySelector(".add-book-btn");
-addButton.addEventListener("click", addNewBook);
+const dialog = document.getElementById("bookDialog");
+const closeDialogButton = document.getElementById("closeDialog");
+const newBookForm = document.getElementById("newBookForm");
+
+addButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeDialogButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+newBookForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pageNo = document.getElementById("pageNo").value;
+  const isRead = document.getElementById("isRead").checked;
+
+  addBookToLibrary(title, author, parseInt(pageNo), isRead);
+  renderLibrary();
+  dialog.close();
+  newBookForm.reset();
+});
 
 renderLibrary();
 
 function renderLibrary() {
-  // Clear existing content in librarySection
   librarySection.innerHTML = "";
 
-  // Iterate through myLibrary and create a new element for each book
   myLibrary.forEach((book) => {
-    // Create a new <div> element for each book
     let newBookElement = document.createElement("div");
     newBookElement.classList.add("book");
-    // Set the innerHTML or textContent of the new element
+
     newBookElement.innerHTML = `
             <h2>${book.title}</h2>
             <p>Author: ${book.author}</p>
             <p>Pages: ${book.pageNo}</p>
             <p>Status: ${book.isRead ? "Read" : "Not read yet"}</p>
         `;
-    // Append the newBookElement to the librarySection
     librarySection.appendChild(newBookElement);
-    // console.log(newBookElement);
   });
-}
-
-function addNewBook() {
-  console.log("clicked new button");
-
-  //use modal form to fetch data from user
-  //call addbooktolibrary with that data.
 }
